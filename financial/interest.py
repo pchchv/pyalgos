f9de9946c9f1cc8104874be4b6a813945e0253fd
pchchv <ipchchv@gmail.com>
2023-04-17 11:12:45 +0300

    Implement a compouns interest

--- financial/interest.py
+++ financial/interest.py
@@ -1,3 +1,10 @@
+"""
+Compound interest: Compound interest is calculated by
+multiplying the original principal amount by one plus the
+annual interest rate increased to the number of compound periods minus one.
+Reference: https://www.investopedia.com/
+"""
+
 from __future__ import annotations
 
 
@@ -35,3 +42,46 @@ def simple_interest(
     if principal <= 0:
         raise ValueError("principal must be > 0")
     return principal * daily_interest_rate * days_between_payments
+
+
+def compound_interest(
+    principal: float,
+    nominal_annual_interest_rate_percentage: float,
+    number_of_compounding_periods: int,
+) -> float:
+    """
+    >>> compound_interest(10000.0, 0.05, 3)
+    1576.2500000000014
+    >>> compound_interest(10000.0, 0.05, 1)
+    500.00000000000045
+    >>> compound_interest(0.5, 0.05, 3)
+    0.07881250000000006
+    >>> compound_interest(10000.0, 0.06, -4)
+    Traceback (most recent call last):
+        ...
+    ValueError: number_of_compounding_periods must be > 0
+    >>> compound_interest(10000.0, -3.5, 3.0)
+    Traceback (most recent call last):
+        ...
+    ValueError: nominal_annual_interest_rate_percentage must be >= 0
+    >>> compound_interest(-5500.0, 0.01, 5)
+    Traceback (most recent call last):
+        ...
+    ValueError: principal must be > 0
+    """
+    if number_of_compounding_periods <= 0:
+        raise ValueError("number_of_compounding_periods must be > 0")
+    if nominal_annual_interest_rate_percentage < 0:
+        raise ValueError("nominal_annual_interest_rate_percentage must be >= 0")  # noqa: E501
+    if principal <= 0:
+        raise ValueError("principal must be > 0")
+
+    return principal * (
+        (1 + nominal_annual_interest_rate_percentage) **
+        number_of_compounding_periods - 1)
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()

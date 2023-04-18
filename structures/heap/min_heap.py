22b2792a5988f4ee9b800bc80a616b19195bd361
pchchv <ipchchv@gmail.com>
2023-04-18 15:10:01 +0300

    Implement a Node class

---
+++ structures/heap/min_heap.py
@@ -0,0 +1,16 @@
+"""
+Min heap data structure
+with decrease key functionality - in O(log(n)) time
+"""
+
+
+class Node:
+    def __init__(self, name, val):
+        self.name = name
+        self.val = val
+
+    def __str__(self):
+        return f"{self.__class__.__name__}({self.name}, {self.val})"
+
+    def __lt__(self, other):
+        return self.val < other.val

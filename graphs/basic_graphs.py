fd7521bc8a4f2c43b64f7fce75ae202f50af5e5b
pchchv <ipchchv@gmail.com>
2023-04-28 14:14:23 +0300

    Implement the Floyd Warshall's algorithm

--- graphs/basic_graphs.py
+++ graphs/basic_graphs.py
@@ -155,3 +155,25 @@ def adjm():
     for _ in range(n):
         a.append(map(int, input().strip().split()))
     return a, n
+
+
+def floy(a_and_n):
+    """
+    Floyd Warshall's algorithm.
+        Args:   G - Dictionary of edges
+                s - Starting Node
+        Vars:   dist - Dictionary storing shortest distance
+                    from s to every other node
+                known - Set of knows nodes
+                path - Preceding node in path
+    """
+    (a, n) = a_and_n
+    dist = list(a)
+    path = [[0] * n for i in range(n)]
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if dist[i][j] > dist[i][k] + dist[k][j]:
+                    dist[i][j] = dist[i][k] + dist[k][j]
+                    path[i][k] = k
+    print(dist)

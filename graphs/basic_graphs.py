84e5ad444579b0f7462378339783005091f19177
pchchv <ipchchv@gmail.com>
2023-04-28 14:10:41 +0300

    Fix

--- graphs/basic_graphs.py
+++ graphs/basic_graphs.py
@@ -40,7 +40,9 @@ def initialize_weighted_undirected_graph(
         graph[i + 1] = []
 
     for e in range(edge_count):
-        x, y, w = (int(i) for i in _input(f"Edge {e + 1}: <node1> <node2> <weight> "))
+        x, y, w = (
+            int(i) for i in _input(f"Edge {e + 1}: <node1> <node2> <weight> ")
+            )
         graph[x].append((y, w))
         graph[y].append((x, w))
     return graph

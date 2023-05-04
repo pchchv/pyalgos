d659e63688730bfba0e158b6320cb6ac12414232
pchchv <ipchchv@gmail.com>
2023-05-04 10:43:30 +0300

    Implement the conversion of celsius to rankine

--- conversions/temperature_conversions.py
+++ conversions/temperature_conversions.py
@@ -53,6 +53,30 @@ def celsius_to_kelvin(celsius: float, ndigits: int = 2) -> float:
     return round(float(celsius) + 273.15, ndigits)
 
 
+def celsius_to_rankine(celsius: float, ndigits: int = 2) -> float:
+    """
+    Convert a given value from Celsius to Rankine and
+    round it to 2 decimal places.
+    Wikipedia reference: https://en.wikipedia.org/wiki/Celsius
+    Wikipedia reference: https://en.wikipedia.org/wiki/Rankine_scale
+    >>> celsius_to_rankine(273.354, 3)
+    983.707
+    >>> celsius_to_rankine(273.354, 0)
+    984.0
+    >>> celsius_to_rankine(0)
+    491.67
+    >>> celsius_to_rankine(20.0)
+    527.67
+    >>> celsius_to_rankine("40")
+    563.67
+    >>> celsius_to_rankine("celsius")
+    Traceback (most recent call last):
+        ...
+    ValueError: could not convert string to float: 'celsius'
+    """
+    return round((float(celsius) * 9 / 5) + 491.67, ndigits)
+
+
 if __name__ == "__main__":
     import doctest
 

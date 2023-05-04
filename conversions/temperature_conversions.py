b3e5fa3fe0dbb922dd4012952b7e809a4fed3619
pchchv <ipchchv@gmail.com>
2023-05-04 10:53:58 +0300

    Implement the conversion of Rankine to Celsius and Fahrenheit

--- conversions/temperature_conversions.py
+++ conversions/temperature_conversions.py
@@ -239,6 +239,50 @@ def kelvin_to_rankine(kelvin: float, ndigits: int = 2) -> float:
     return round((float(kelvin) * 9 / 5), ndigits)
 
 
+def rankine_to_celsius(rankine: float, ndigits: int = 2) -> float:
+    """
+    Convert a given value from Rankine to Celsius and
+    round it to 2 decimal places.
+    Wikipedia reference: https://en.wikipedia.org/wiki/Rankine_scale
+    Wikipedia reference: https://en.wikipedia.org/wiki/Celsius
+    >>> rankine_to_celsius(273.354, 3)
+    -121.287
+    >>> rankine_to_celsius(273.354, 0)
+    -121.0
+    >>> rankine_to_celsius(273.15)
+    -121.4
+    >>> rankine_to_celsius(300)
+    -106.48
+    >>> rankine_to_celsius("315.5")
+    -97.87
+    >>> rankine_to_celsius("rankine")
+    Traceback (most recent call last):
+        ...
+    ValueError: could not convert string to float: 'rankine'
+    """
+    return round((float(rankine) - 491.67) * 5 / 9, ndigits)
+
+
+def rankine_to_fahrenheit(rankine: float, ndigits: int = 2) -> float:
+    """
+    Convert a given value from Rankine to Fahrenheit and
+    round it to 2 decimal places.
+    Wikipedia reference: https://en.wikipedia.org/wiki/Rankine_scale
+    Wikipedia reference: https://en.wikipedia.org/wiki/Fahrenheit
+    >>> rankine_to_fahrenheit(273.15)
+    -186.52
+    >>> rankine_to_fahrenheit(300)
+    -159.67
+    >>> rankine_to_fahrenheit("315.5")
+    -144.17
+    >>> rankine_to_fahrenheit("rankine")
+    Traceback (most recent call last):
+        ...
+    ValueError: could not convert string to float: 'rankine'
+    """
+    return round(float(rankine) - 459.67, ndigits)
+
+
 if __name__ == "__main__":
     import doctest
 

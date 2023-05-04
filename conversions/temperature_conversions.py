ebcf05fc87e53a0bdca2bb60d7a5b5ba1eb0afa7
pchchv <ipchchv@gmail.com>
2023-05-04 11:02:44 +0300

    Implement the conversion of Reaumur to Rankine

--- conversions/temperature_conversions.py
+++ conversions/temperature_conversions.py
@@ -360,6 +360,25 @@ def reaumur_to_celsius(reaumur: float, ndigits: int = 2) -> float:
     return round((float(reaumur) * 1.25), ndigits)
 
 
+def reaumur_to_rankine(reaumur: float, ndigits: int = 2) -> float:
+    """
+    Convert a given value from Reaumur to Rankine and
+    round it to 2 decimal places.
+    Reference:- http://www.csgnetwork.com/temp2conv.html
+    >>> reaumur_to_rankine(0)
+    491.67
+    >>> reaumur_to_rankine(20.0)
+    536.67
+    >>> reaumur_to_rankine(40)
+    581.67
+    >>> reaumur_to_rankine("reaumur")
+    Traceback (most recent call last):
+        ...
+    ValueError: could not convert string to float: 'reaumur'
+    """
+    return round((float(reaumur) * 2.25 + 32 + 459.67), ndigits)
+
+
 if __name__ == "__main__":
     import doctest
 
